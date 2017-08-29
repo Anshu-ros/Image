1e28fadc378efda3ecc91a832fb1004cffe55880
Anshu <anshuyadavros@gmail.com>
2017-08-29 20:08:27 +0530

    modified color detector

--- ColorDetect.py
+++ ColorDetect.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
 while(1):
-    frame=cap.read()
+    ret, frame=cap.read()
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     lower_blue=np.array([110,50,50])
     upper_blue=np.array([30,255,255])
