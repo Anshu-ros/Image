c6c078b0171098ded1b142127ba6f1a382256381
Anshu <anshuyadavros@gmail.com>
2017-09-02 21:30:51 +0530

    Modified ImagePyramid and Pyramid.jpg deleted

--- ImagePyramid.py
+++ ImagePyramid.py
@@ -1,15 +1,8 @@
 import cv2
 import numpy as np,sys
-A = cv2.imread('Anshu.jpg')
-G = A.copy()
-gpA = [G]
-for i in xrange(6):
-    G = cv2.pyrDown(G)
-    gpA.append(G)
 
-ls = gpA[0]
-for i in xrange(1,6):
-    ls = cv2.pyrUp(ls)
-
-cv2.imwrite('Pyramid.jpg',ls)
+image = cv2.imread('Anshu'.jpg)
+G=image.copy()
+G=cv2.pyrDown(G)
+cv2.imwrite('Pyramid.jpg',G)
                  
